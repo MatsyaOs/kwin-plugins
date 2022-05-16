007c01c39c186234535e82cc69dcc246fb49c7f3
Abhishek Sharma <sabhiron5@gmail.com>
2022-05-16 07:11:56 +0530

    matyou

---
+++ plugins/decoration/build/CMakeFiles/cutefishdecoration.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tokyo/cuteclone/kwin-plugins/plugins/decoration/button.cpp" "/home/tokyo/cuteclone/kwin-plugins/plugins/decoration/build/CMakeFiles/cutefishdecoration.dir/button.o"
+  "/home/tokyo/cuteclone/kwin-plugins/plugins/decoration/decoration.cpp" "/home/tokyo/cuteclone/kwin-plugins/plugins/decoration/build/CMakeFiles/cutefishdecoration.dir/decoration.o"
+  "/home/tokyo/cuteclone/kwin-plugins/plugins/decoration/x11shadow.cpp" "/home/tokyo/cuteclone/kwin-plugins/plugins/decoration/build/CMakeFiles/cutefishdecoration.dir/x11shadow.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "KCOREADDONS_LIB"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  "QT_X11EXTRAS_LIB"
+  "QT_XML_LIB"
+  "cutefishdecoration_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtX11Extras"
+  "/usr/include/KF5/KConfigCore"
+  "/usr/include/KF5"
+  "/usr/include/KF5/KConfigGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtXml"
+  "/usr/include/KF5/KCoreAddons"
+  "/usr/include/KF5/KWindowSystem"
+  "/usr/include/KDecoration2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ plugins/decoration/build/CMakeFiles/cutefishdecoration.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cutefishdecoration.dir/button.o"
+  "CMakeFiles/cutefishdecoration.dir/decoration.o"
+  "CMakeFiles/cutefishdecoration.dir/x11shadow.o"
+  "libcutefishdecoration.pdb"
+  "libcutefishdecoration.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cutefishdecoration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
